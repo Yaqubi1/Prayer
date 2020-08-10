48682fa3352ebfd6bdc6f9796500aee0816c523c
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-10 16:31:11 +0430

    some JS

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let rakat = [1, 2, 3, 4];
+let boner = ['Morgonbönen', 'Middagsbönen', 'Eftermiddagsbönen', 'Solnedgångsbönen', 'Kvällsbönen'];
+
+// add 1 on the filename and target iframe
+function nextClick() {
+    
+}
+
+// subtract 1 on the filename and target iframe
+function backClick() {
+    
+}
+
+// target iframe with the current number in the iframe
+function swedishClick() {
+    
+}
+
+// target iframe with the current number in the iframe
+function transliterationClick() {
+    
+}
+
+// target iframe with the current number in the iframe
+function arabicClick() {
+    
+}
