f5d11fb90ed58c17eaa45680dbe3ecd08f04fe19
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-10 17:43:29 +0430

    onClick code for back & next buttons

--- js/script.js
+++ js/script.js
@@ -7,17 +7,19 @@ let arabicCounter = 1;
 function nextClick() {
     swedishCounter++;
     arabicCounter++;
+    changeToCurrentPage();
 }
 
 // subtract 1 on the filename and target iframe
 function backClick() {
     swedishCounter--;
     arabicCounter--;
+    changeToCurrentPage();
 }
 
 // target iframe with the current number in the iframe
 function swedishClick() {
-    document.getElementById("myiframe").src = "svenska" + swedishCounter + ".htm";
+    document.getElementById("myiframe").src = "sven" + swedishCounter + ".htm";
 }
 
 // // target iframe with the current number in the iframe
@@ -27,5 +29,16 @@ function swedishClick() {
 
 // target iframe with the current number in the iframe
 function arabicClick() {
-    document.getElementById("myiframe").src = "arabiska" + arabicCounter + ".htm";
+    document.getElementById("myiframe").src = "arab" + arabicCounter + ".htm";
+}
+
+function changeToCurrentPage() {
+    let determineSource = document.getElementById("myiframe").src;
+    if (determineSource.charAt(determineSource.length - 9) === "a") {
+        document.getElementById("myiframe").src = "arab" + swedishCounter + ".htm";
+    } else if (determineSource.charAt(determineSource.length - 9) === "s") {
+        document.getElementById("myiframe").src = "sven" + swedishCounter + ".htm";
+    } else if (determineSource.charAt(determineSource.length - 9) === "t") {
+        document.getElementById("myiframe").src = "tran" + swedishCounter + ".htm";
+    }
 }
