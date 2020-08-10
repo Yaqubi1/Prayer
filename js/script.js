55acbdda02f9842317d9d23f6c107a2e83aa855a
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-10 17:49:03 +0430

    optimized js for all buttons

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
 let rakat = [1, 2, 3, 4];
 let boner = ['Morgonbönen', 'Middagsbönen', 'Eftermiddagsbönen', 'Solnedgångsbönen', 'Kvällsbönen'];
-let swedishCounter = 1;
-let arabicCounter = 1;
+let pageCounter = 1;
 
 // add 1 on the filename and target iframe
 function nextClick() {
-    swedishCounter++;
-    arabicCounter++;
+    pageCounter++;
     changeToCurrentPage();
 }
 
 // subtract 1 on the filename and target iframe
 function backClick() {
-    swedishCounter--;
-    arabicCounter--;
+    pageCounter--;
     changeToCurrentPage();
 }
 
 // target iframe with the current number in the iframe
 function swedishClick() {
-    document.getElementById("myiframe").src = "sven" + swedishCounter + ".htm";
+    document.getElementById("myiframe").src = "sven" + pageCounter + ".htm";
 }
 
-// // target iframe with the current number in the iframe
-// function transliterationClick() {
-    
-// }
+// target iframe with the current number in the iframe
+function transliterationClick() {
+    document.getElementById("myiframe").src = "tran" + pageCounter + ".htm";
+}
 
 // target iframe with the current number in the iframe
 function arabicClick() {
-    document.getElementById("myiframe").src = "arab" + arabicCounter + ".htm";
+    document.getElementById("myiframe").src = "arab" + pageCounter + ".htm";
 }
 
 function changeToCurrentPage() {
     let determineSource = document.getElementById("myiframe").src;
     if (determineSource.charAt(determineSource.length - 9) === "a") {
-        document.getElementById("myiframe").src = "arab" + swedishCounter + ".htm";
+        document.getElementById("myiframe").src = "arab" + pageCounter + ".htm";
     } else if (determineSource.charAt(determineSource.length - 9) === "s") {
-        document.getElementById("myiframe").src = "sven" + swedishCounter + ".htm";
+        document.getElementById("myiframe").src = "sven" + pageCounter + ".htm";
     } else if (determineSource.charAt(determineSource.length - 9) === "t") {
-        document.getElementById("myiframe").src = "tran" + swedishCounter + ".htm";
+        document.getElementById("myiframe").src = "tran" + pageCounter + ".htm";
     }
 }
