f93e0936ca5d94737996e74807d9dfe4a6c1119b
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-10 17:26:58 +0430

    linked js, onclick functions

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 let rakat = [1, 2, 3, 4];
 let boner = ['Morgonbönen', 'Middagsbönen', 'Eftermiddagsbönen', 'Solnedgångsbönen', 'Kvällsbönen'];
+let swedishCounter = 1;
+let arabicCounter = 1;
 
 // add 1 on the filename and target iframe
 function nextClick() {
-    
+    swedishCounter++;
+    arabicCounter++;
 }
 
 // subtract 1 on the filename and target iframe
 function backClick() {
-    
+    swedishCounter--;
+    arabicCounter--;
 }
 
 // target iframe with the current number in the iframe
 function swedishClick() {
-    
+    document.getElementById("myiframe").src = "svenska" + swedishCounter + ".htm";
 }
 
-// target iframe with the current number in the iframe
-function transliterationClick() {
+// // target iframe with the current number in the iframe
+// function transliterationClick() {
     
-}
+// }
 
 // target iframe with the current number in the iframe
 function arabicClick() {
-    
+    document.getElementById("myiframe").src = "arabiska" + arabicCounter + ".htm";
 }
