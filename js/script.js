dc2d4fccbd8561d4520ee9436f8116719e2c3fbc
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-08-18 03:29:01 +0430

    js rakah and position counter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-let rakat = [1, 2, 3, 4];
+let rakahcounter = 1;
 let boner = ['Morgonbönen', 'Middagsbönen', 'Eftermiddagsbönen', 'Solnedgångsbönen', 'Kvällsbönen'];
 let pageCounter = 1;
+let positionCounter = 1;
 
 // add 1 on the filename and target iframe
 function nextClick() {
@@ -11,7 +12,18 @@ function nextClick() {
             changeSound();
         }
     }
-    
+    if (pageCounter === 7 || pageCounter === 14) {
+        rakahcounter++;
+        changeRakah();
+    }
+    if (pageCounter === 4 || pageCounter === 5 || pageCounter === 9 || pageCounter === 10 || pageCounter === 12) {
+        positionCounter++;
+        changePosition();
+    }
+    if (pageCounter === 7) {
+        positionCounter -= 2;
+        changePosition();
+    }
 }
 
 // subtract 1 on the filename and target iframe
@@ -21,7 +33,42 @@ function backClick() {
         changePage();
         changeSound();
     }
-    
+    if (pageCounter === 6 || pageCounter === 13) {
+        rakahcounter--;
+        changeRakah();
+    } 
+    if (pageCounter === 3 || pageCounter === 4 || pageCounter === 8 || pageCounter === 9 || pageCounter === 11) {
+        positionCounter--;
+        changePosition();
+    } 
+    if (pageCounter === 6) {
+        positionCounter += 2;
+        changePosition();
+    }
+}
+
+function changePosition() {
+    if (positionCounter === 1) {
+        document.getElementById("position").innerHTML = "Stå upp";
+    } else if (positionCounter === 2) {
+        document.getElementById("position").innerHTML = "Buga";
+    } else if (positionCounter === 3) {
+        document.getElementById("position").innerHTML = "Buga mot marken";
+    } else if (positionCounter === 4) {
+        document.getElementById("position").innerHTML = "Sitt";
+    }
+}
+
+function changeRakah() {
+    if (rakahcounter === 1) {
+        document.getElementById("rakahnumber").innerHTML = "1";
+    } else if (rakahcounter === 2) {
+        document.getElementById("rakahnumber").innerHTML = "2";
+    } else if (rakahcounter === 3) {
+        document.getElementById("rakahnumber").innerHTML = "3";
+    } else if (rakahcounter === 4) {
+        document.getElementById("rakahnumber").innerHTML = "4";
+    }
 }
 
 // target iframe with the current number in the iframe
